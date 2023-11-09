3235bba93c3207e2e4ca514659a568b3b2345b63
FihFafnir <fihfdragon@gmail.com>
2023-11-09 06:35:04 -0300

    6 Sensors MazeSolver Inicial Commit

--- MazeSolver/FiveSensors/FiveSensors.ino
+++ MazeSolver/FiveSensors/FiveSensors.ino
@@ -39,12 +39,12 @@ void setup() {
 
 void loop() {
     if (
-        digitalRead(LEFT_OUTER_SENSOR_PIN) && 
-        digitalRead(LEFT_INNER_SENSOR_PIN) && 
-        digitalRead(MIDDLE_SENSOR_PIN) && 
-        digitalRead(RIGHT_INNER_SENSOR_PIN) && 
-        digitalRead(RIGHT_OUTER_SENSOR_PIN)
+        !digitalRead(LEFT_OUTER_SENSOR_PIN) && 
+        !digitalRead(LEFT_INNER_SENSOR_PIN) && 
+        !digitalRead(MIDDLE_SENSOR_PIN) && 
+        !digitalRead(RIGHT_INNER_SENSOR_PIN) && 
+        !digitalRead(RIGHT_OUTER_SENSOR_PIN)
     )
-        ms->followLine();   
-    else ms->stop();
+        ms->stop();   
+    else ms->followLine();
 }
